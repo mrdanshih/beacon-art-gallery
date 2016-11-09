4bc5ffed6f1d164f0d50c4660b514aebdeffedbf
Daniel Shih <danielshihtiger@gmail.com>
2016-11-08 16:25:51 -0800

    Bug fixes, UI updates

---
+++ app/src/main/java/com/UciStudentCenterAndEventServices/ArtGallery/ArtBeaconsActivity.java
@@ -0,0 +1,262 @@
+package com.UciStudentCenterAndEventServices.ArtGallery;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.estimote.sdk.Beacon;
+import com.estimote.sdk.BeaconManager;
+import com.estimote.sdk.Region;
+import com.estimote.sdk.SystemRequirementsChecker;
+
+
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+import static com.UciStudentCenterAndEventServices.ArtGallery.R.drawable.beacon;
+import static com.UciStudentCenterAndEventServices.ArtGallery.R.drawable.no_image;
+
+
+public class ArtBeaconsActivity extends AppCompatActivity implements ExhibitConnection.AsyncResponse{
+
+    private static final String TAG = "ArtBeaconsActivity";
+
+    private BeaconManager beaconManager;
+    private Region region;
+
+
+    String artGalleryUUID = "B9407F30-F5F8-466E-AFF9-25556B57FE6D";
+
+    ArrayList<ExhibitPiece> piecesList;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_art_gallery);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.artGalleryToolbar);
+        setSupportActionBar(toolbar);
+
+        new ExhibitConnection(this).execute();
+        doBeaconSearching();
+
+
+    }
+
+    private void doBeaconSearching(){
+        beaconManager = new BeaconManager(this);
+        beaconManager.setRangingListener(new BeaconManager.RangingListener() {
+            String artistName;
+            String pieceTitle;
+            String pieceInfo;
+            String beaconID;
+            String imageURL;
+            Beacon currentBeacon, previousClosest;
+
+            int beaconCredibility = 0;
+
+            @Override
+            public void onBeaconsDiscovered(Region region, List<Beacon> beaconList) {
+                if(!beaconList.isEmpty()) {
+                    Beacon nearestBeacon = beaconList.get(0);
+
+                    //If discovered new closest Beacon...
+                    //Basic beacon "credibility" test - If same new Beacon is seen for 2 cycles,
+                    //that Beacon will be seen as a credible new Beacon.
+                    if(!nearestBeacon.equals(currentBeacon)){
+                        if(nearestBeacon.equals(previousClosest)){
+                            if(beaconCredibility == 2) {
+                                beaconCredibility = 0;
+
+                                currentBeacon = nearestBeacon;
+                                System.out.println("Credible new Beacon: " + nearestBeacon.getMajor());
+
+                                ExhibitPiece artPiece = getPieceInfo(nearestBeacon);
+                                artistName = artPiece.artistName;
+                                pieceTitle = artPiece.title;
+                                pieceInfo = artPiece.blurb;
+                                beaconID = artPiece.beaconMajorId + "";
+                                imageURL = artPiece.pictureUrl;
+
+                                System.out.println(imageURL);
+
+                                ((TextView) findViewById(R.id.artistName)).setText(artistName);
+                                ((TextView) findViewById(R.id.beaconID)).setText(beaconID);
+                                ((TextView) findViewById(R.id.artPieceName)).setText(pieceTitle);
+                                ((TextView) findViewById(R.id.artPieceInfo)).setText(pieceInfo);
+
+                                DownloadImageTask task = new DownloadImageTask((ImageView) findViewById(R.id.artPieceImage));
+                                task.execute(imageURL);
+                            }else{
+                                beaconCredibility++;
+                                System.out.println("Updating credibility of " + nearestBeacon.getMajor() + " to " + beaconCredibility);
+                            }
+
+                        }else{
+                            System.out.println("New Beacon: " + nearestBeacon.getMajor());
+
+                            String newID = "Found " + getPieceInfo(nearestBeacon).beaconMajorId + " as new closest art piece...";
+                            ((TextView) findViewById(R.id.beaconID)).setText(newID);
+
+                            beaconCredibility = 0;
+                            previousClosest = nearestBeacon;
+                        }
+
+
+                        //If it's the same nearest beacon, no need to do anything!
+                    }else{
+                        System.out.println("Same beacon: Major ID is " + nearestBeacon.getMajor());
+                        ((TextView) findViewById(R.id.beaconID)).setText(beaconID);
+                        previousClosest = nearestBeacon;
+
+                    }
+
+                }else{
+                    System.out.println("No beacons in range.");
+
+                    artistName = "No art piece in range!";
+                    pieceTitle = "Walk up to an art piece.";
+                    pieceInfo = "Searching for art piece beacons...";
+                    beaconID = "";
+                    imageURL = "";
+
+                    ((ImageView) findViewById(R.id.artPieceImage)).setImageResource(beacon);
+                    ((TextView) findViewById(R.id.artistName)).setText(artistName);
+                    ((TextView) findViewById(R.id.beaconID)).setText(beaconID);
+                    ((TextView) findViewById(R.id.artPieceName)).setText(pieceTitle);
+                    ((TextView) findViewById(R.id.artPieceInfo)).setText(pieceInfo);
+
+                }
+
+
+            }
+        });
+
+        region = new Region("Art Gallery Region", UUID.fromString(artGalleryUUID), null, null);
+    }
+
+
+
+    @Override
+    public void processFinish(ArrayList<ExhibitPiece> result){
+        piecesList = result;
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if (!SystemRequirementsChecker.checkWithDefaultDialogs(this)) {
+            Log.e(TAG, "Can't scan for beacons, some pre-conditions were not met");
+            Log.e(TAG, "Read more about what's required at: http://estimote.github.io/Android-SDK/JavaDocs/com/estimote/sdk/SystemRequirementsChecker.html");
+            Log.e(TAG, "If this is fixable, you should see a popup on the app's screen right now, asking to enable what's necessary");
+        } else if(beaconManager != null) {
+            Log.d(TAG, "Starting BeaconManager ranging.");
+
+            beaconManager.connect(new BeaconManager.ServiceReadyCallback() {
+                @Override
+                //Starts the ranging of the beacons
+                public void onServiceReady() {
+                    beaconManager.startRanging(region);
+                }
+            });
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        if(beaconManager != null) {
+            beaconManager.stopRanging(region);
+            Log.d(TAG, "Stopping Beacon ranging.");
+        }
+
+        super.onPause();
+
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+
+
+
+    private ExhibitPiece getPieceInfo(Beacon beaconDetails){
+        ExhibitPiece associatedPiece = new ExhibitPiece(0, 0, "Unknown Piece!", 0,
+                                                        "Major ID is " + beaconDetails.getMajor(),
+                                                        "","","","",true,0);
+
+        for(ExhibitPiece piece: piecesList){
+            if (piece.beaconMajorId == beaconDetails.getMajor()){
+                associatedPiece = piece;
+                break;
+            }
+        }
+
+        return associatedPiece;
+
+    }
+
+
+    private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
+        ImageView bmImage;
+
+        public DownloadImageTask(ImageView bmImage) {
+            this.bmImage = bmImage;
+        }
+
+        protected void onPreExecute() {
+            bmImage.setVisibility(View.INVISIBLE);
+            findViewById(R.id.loadImageSpinner).setVisibility(View.VISIBLE);
+        }
+
+        protected Bitmap doInBackground(String[] url) {
+            //There's only one URL - the one passed in.
+            String urldisplay = url[0];
+
+            Bitmap imageBitmap = null;
+
+            try {
+                InputStream in = new java.net.URL(urldisplay).openStream();
+                imageBitmap = BitmapFactory.decodeStream(in);
+
+            } catch (Exception e) {
+                Log.e("Error", e.getMessage());
+                e.printStackTrace();
+
+            }
+            return imageBitmap;
+        }
+
+        protected void onPostExecute(Bitmap result) {
+            if(result != null) {
+                bmImage.setImageBitmap(result);
+            }else{
+                bmImage.setImageResource(no_image);
+            }
+
+            findViewById(R.id.loadImageSpinner).setVisibility(View.INVISIBLE);
+            bmImage.setVisibility(View.VISIBLE);
+        }
+    }
+
+
+
+
+}
+
+
+
--- app/src/main/java/com/UciStudentCenterAndEventServices/ArtGallery/MainActivity.java
+++ app/src/main/java/com/UciStudentCenterAndEventServices/ArtGallery/MainActivity.java
@@ -2,276 +2,64 @@ package com.UciStudentCenterAndEventServices.ArtGallery;
 
 import android.content.Context;
 import android.content.DialogInterface;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
+import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
-import android.os.AsyncTask;
-import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.widget.ImageView;
-import android.widget.TextView;
 
-import com.estimote.sdk.Beacon;
-import com.estimote.sdk.BeaconManager;
-import com.estimote.sdk.Region;
-import com.estimote.sdk.SystemRequirementsChecker;
-
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
-
-import static com.UciStudentCenterAndEventServices.ArtGallery.R.drawable.beacon;
-import static com.UciStudentCenterAndEventServices.ArtGallery.R.drawable.no_image;
-
-
-public class MainActivity extends AppCompatActivity implements ExhibitConnection.AsyncResponse{
-
-    private static final String TAG = "MainActivity";
-
-    private BeaconManager beaconManager;
-    private Region region;
-
-
-    String artGalleryUUID = "B9407F30-F5F8-466E-AFF9-25556B57FE6D";
-
-    ArrayList<ExhibitPiece> piecesList;
-
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        internetCheck();
+
+
+    }
+
+    private void internetCheck(){
         //Check if app has network connectivity...
         if(isNetworkAvailable()){
             System.out.println("CONNECTED TO INTERNET");
             //Does the fetching of the database (network process) using Async task
-            new ExhibitConnection(this).execute();
-            doBeaconSearching();
+
 
         }else{
             System.out.println("NO INTERNET CONNECTION");
             final AlertDialog noInternetDialog = new AlertDialog.Builder(this).create();
             noInternetDialog.setTitle("No internet connection.");
             noInternetDialog.setMessage("This app requires an internet connection to function. Connect to the internet and try again.");
-            noInternetDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK", new DialogInterface.OnClickListener() {
+            noInternetDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "Try again", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    internetCheck();
+                }
+            });
+            noInternetDialog.setButton(AlertDialog.BUTTON_NEGATIVE, "Quit app", new DialogInterface.OnClickListener() {
                 @Override
                 public void onClick(DialogInterface dialogInterface, int i) {
                     finish();
                 }
             });
+
             noInternetDialog.show();
 
         }
+    }
+
+    public void startArtGallery(View view){
+        Intent artGalleryIntent = new Intent(this, ArtBeaconsActivity.class);
+        startActivity(artGalleryIntent);
 
     }
 
-    private void doBeaconSearching(){
-        beaconManager = new BeaconManager(this);
-        beaconManager.setRangingListener(new BeaconManager.RangingListener() {
-            String artistName;
-            String pieceTitle;
-            String pieceInfo;
-            String beaconID;
-            String imageURL;
-            Beacon currentBeacon, previousClosest;
-
-            int beaconCredibility = 0;
-
-            @Override
-            public void onBeaconsDiscovered(Region region, List<Beacon> beaconList) {
-                if(!beaconList.isEmpty()) {
-                    Beacon nearestBeacon = beaconList.get(0);
-
-                    //If discovered new closest Beacon...
-                    //Basic beacon "credibility" test - If same new Beacon is seen for 2 cycles,
-                    //that Beacon will be seen as a credible new Beacon.
-                    if(!nearestBeacon.equals(currentBeacon)){
-                        if(nearestBeacon.equals(previousClosest)){
-                            if(beaconCredibility == 2) {
-                                beaconCredibility = 0;
-
-                                currentBeacon = nearestBeacon;
-                                System.out.println("Credible new Beacon: " + nearestBeacon.getMajor());
-
-                                ExhibitPiece artPiece = getPieceInfo(nearestBeacon);
-                                artistName = artPiece.artistName;
-                                pieceTitle = artPiece.title;
-                                pieceInfo = artPiece.blurb;
-                                beaconID = artPiece.beaconMajorId + "";
-                                imageURL = artPiece.pictureUrl;
-
-                                System.out.println(imageURL);
-
-                                ((TextView) findViewById(R.id.artistName)).setText(artistName);
-                                ((TextView) findViewById(R.id.beaconID)).setText(beaconID);
-                                ((TextView) findViewById(R.id.artPieceName)).setText(pieceTitle);
-                                ((TextView) findViewById(R.id.artPieceInfo)).setText(pieceInfo);
-
-                                DownloadImageTask task = new DownloadImageTask((ImageView) findViewById(R.id.artPieceImage));
-                                task.execute(imageURL);
-                            }else{
-                                beaconCredibility++;
-                                System.out.println("Updating credibility of " + nearestBeacon.getMajor() + " to " + beaconCredibility);
-                            }
-
-                        }else{
-                            System.out.println("New Beacon: " + nearestBeacon.getMajor());
-
-                            String newID = "Found " + getPieceInfo(nearestBeacon).beaconMajorId + " as new closest art piece...";
-                            ((TextView) findViewById(R.id.beaconID)).setText(newID);
-
-                            beaconCredibility = 0;
-                            previousClosest = nearestBeacon;
-                        }
-
-
-                        //If it's the same nearest beacon, no need to do anything!
-                    }else{
-                        System.out.println("Same beacon: Major ID is " + nearestBeacon.getMajor());
-                        ((TextView) findViewById(R.id.beaconID)).setText(beaconID);
-                        previousClosest = nearestBeacon;
-
-                    }
-
-                }else{
-                    System.out.println("No beacons in range.");
-
-                    artistName = "No art piece in range!";
-                    pieceTitle = "Walk up to an art piece.";
-                    pieceInfo = "Searching for art piece beacons...";
-                    beaconID = "";
-                    imageURL = "";
-
-                    ((TextView) findViewById(R.id.artistName)).setText(artistName);
-                    ((TextView) findViewById(R.id.beaconID)).setText(beaconID);
-                    ((TextView) findViewById(R.id.artPieceName)).setText(pieceTitle);
-                    ((TextView) findViewById(R.id.artPieceInfo)).setText(pieceInfo);
-
-                }
-
-
-            }
-        });
-
-        region = new Region("Art Gallery Region", UUID.fromString(artGalleryUUID), null, null);
-    }
-
-
-
-    @Override
-    public void processFinish(ArrayList<ExhibitPiece> result){
-        piecesList = result;
-    }
-
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-
-        if (!SystemRequirementsChecker.checkWithDefaultDialogs(this)) {
-            Log.e(TAG, "Can't scan for beacons, some pre-conditions were not met");
-            Log.e(TAG, "Read more about what's required at: http://estimote.github.io/Android-SDK/JavaDocs/com/estimote/sdk/SystemRequirementsChecker.html");
-            Log.e(TAG, "If this is fixable, you should see a popup on the app's screen right now, asking to enable what's necessary");
-        } else if(beaconManager != null) {
-            Log.d(TAG, "Starting BeaconManager ranging.");
-
-            beaconManager.connect(new BeaconManager.ServiceReadyCallback() {
-                @Override
-                //Starts the ranging of the beacons
-                public void onServiceReady() {
-                    beaconManager.startRanging(region);
-                }
-            });
-        }
-    }
-
-    @Override
-    protected void onPause() {
-        if(beaconManager != null) {
-            beaconManager.stopRanging(region);
-            Log.d(TAG, "Stopping Beacon ranging.");
-        }
-
-        super.onPause();
-
-
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-    }
-
-
-
-
-    private ExhibitPiece getPieceInfo(Beacon beaconDetails){
-        ExhibitPiece associatedPiece = new ExhibitPiece(0, 0, "Unknown Piece!", 0,
-                                                        "Major ID is " + beaconDetails.getMajor(),
-                                                        "","","","",true,0);
-
-        for(ExhibitPiece piece: piecesList){
-            if (piece.beaconMajorId == beaconDetails.getMajor()){
-                associatedPiece = piece;
-                break;
-            }
-        }
-
-        return associatedPiece;
-
-    }
-
-
-    private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
-        ImageView bmImage;
-
-        public DownloadImageTask(ImageView bmImage) {
-            this.bmImage = bmImage;
-        }
-
-        protected void onPreExecute() {
-            bmImage.setVisibility(View.INVISIBLE);
-            findViewById(R.id.loadImageSpinner).setVisibility(View.VISIBLE);
-        }
-
-        protected Bitmap doInBackground(String[] url) {
-            //There's only one URL - the one passed in.
-            String urldisplay = url[0];
-
-            Bitmap imageBitmap = null;
-
-            try {
-                InputStream in = new java.net.URL(urldisplay).openStream();
-                imageBitmap = BitmapFactory.decodeStream(in);
-
-            } catch (Exception e) {
-                Log.e("Error", e.getMessage());
-                e.printStackTrace();
-
-            }
-            return imageBitmap;
-        }
-
-        protected void onPostExecute(Bitmap result) {
-            if(result != null) {
-                bmImage.setImageBitmap(result);
-            }else{
-                bmImage.setImageResource(no_image);
-            }
-
-            findViewById(R.id.loadImageSpinner).setVisibility(View.INVISIBLE);
-            bmImage.setVisibility(View.VISIBLE);
-        }
-    }
 
     private boolean isNetworkAvailable() {
         ConnectivityManager connectivityManager
@@ -281,8 +69,4 @@ public class MainActivity extends AppCompatActivity implements ExhibitConnection
         return activeNetworkInfo != null;
     }
 
-
 }
-
-
-
